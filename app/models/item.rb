38a5bfcae8aa650f64b93f271bcff9bf9f30461d
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-02-20 22:13:14 +0900

    ActiveHashバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   validates :title, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, :price, presence: true
+  validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, numericality: { other_than: 1 }
 
   belongs_to :user
   has_one :payment
