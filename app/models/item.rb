1db3b90eca0564d6233afe64a6ae2a4e80df925e
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-02-21 18:20:23 +0900

    価格についてのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   validates :title, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, :price, presence: true
   validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, numericality: { other_than: 1 }
 
+  PRICE_REGEX = /\A(?=.*?[\d])[300-9999999]+\z/.freeze
+  validates_format_of :price, with: PRICE_REGEX, message: 'は半角数字を使用し、300から9,999,999の間で入力してください'
+
   belongs_to :user
   has_one :payment
   has_one_attached :image
