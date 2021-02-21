c66fa4591423d1573658f38268ef32503e4ea668
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-02-22 00:43:34 +0900

    バリデーション追加修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,8 @@
 class Item < ApplicationRecord
-  validates :title, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, :price, presence: true
+  validates :title, :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, :price, :image, presence: true
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, numericality: { other_than: 1 }
-
-  PRICE_REGEX = /\A(?=.*?[\d])[300-9999999]+\z/.freeze
-  validates_format_of :price, with: PRICE_REGEX, message: 'は半角数字を使用し、300から9,999,999の間で入力してください'
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は半角数字を使用し、300から9,999,999の間で入力してください' }
 
   belongs_to :user
   has_one :payment
