b3ffbb010116471b723853a87df1fa1bcc7ce711
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-02-22 21:31:12 +0900

    [item単体テスト]テスト用画像・テスト用インスタンス準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title           { Faker::Lorem.word }
+    description     { Faker::Lorem.paragraph }
+    category_id     { 2 }
+    condition_id    { 2 }
+    postage_id      { 2 }
+    prefecture_id   { 2 }
+    shipment_day_id { 2 }
+    price           { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
