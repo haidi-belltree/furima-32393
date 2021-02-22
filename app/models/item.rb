b6f032cf9fde4d45b0ab707fb43d36ce3ce68919
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-02-22 23:11:20 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,20 @@
 class Item < ApplicationRecord
-  validates :title, :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, :price, :image, presence: true
+  validates :title, :image, presence: true
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipment_day_id, numericality: { other_than: 1 }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は半角数字を使用し、300から9,999,999の間で入力してください' }
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :shipment_day_id
+  end
+
+  with_options presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は半角数字を使用し、300から9,999,999の間で入力してください' } do
+    validates :price
+  end
 
   belongs_to :user
-  has_one :payment
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
