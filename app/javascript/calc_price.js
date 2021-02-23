249eac0fdf78adefc4df192645a564c9b59ee0c1
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-02-23 12:01:20 +0900

    商品出品ページの販売手数料・販売利益を整数表示に修正

--- app/javascript/calc_price.js
+++ app/javascript/calc_price.js
@@ -2,7 +2,7 @@ function calc (){
   const itemPrice = document.getElementById('item-price');
   itemPrice.addEventListener('keyup', () => {
     const priceVal = itemPrice.value;
-    const priceTax = priceVal * 0.1;
+    const priceTax = Math.round(priceVal * 0.1);
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${priceTax}`;
     const profit = document.getElementById("profit");
