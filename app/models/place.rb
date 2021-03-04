12223514e7dcbde015d036a1a61937989c6331f5
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-03-04 22:37:01 +0900

    placeモデルのバリデーション修正

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,7 +5,7 @@ class Place < ApplicationRecord
     validates :block, format: { with: /\A[ぁ-んァ-ン一-龥々a-zA-Z0-9-ー]/, message: "は全角日本語および半角英数で記入してください" }
     validates :phone_number, format: { with: /\A[0-9]{10, 11}\z/, message: "は半角数字（ハイフンなし）で記入してください" }
   end
-  validates :prefecture, numericality: { other_than: 0, message: "を選択してください" }
+  validates :prefecture, numericality: { other_than: 1, message: "を選択してください" }
   validates :building, format: { with: /\A[ぁ-んァ-ン一-龥々a-zA-Z0-9-ー]/, message: "は全角日本語および半角英数で記入してください" }
 
   belongs_to :payment
