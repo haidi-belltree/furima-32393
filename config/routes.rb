00056fc54edb50f780b279bc52ff8db4fa8da218
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-03-05 01:25:02 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'payments/new'
   devise_for :users
   root to: "items#index"
 
-  resources :items
-  resources :payments, only: [:new, :create]
+  resources :items do
+    resources :payments, only: [:new, :create]
+  end
 end
