316a57381745d5293dc5ec2bfb87e3f20becc7be
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-03-07 11:26:16 +0900

    購入内容および配送先情報を保存できるように実装

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,5 +5,20 @@ class PaymentsController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @payment_place = PaymentPlace.new(payment_params)
+    if @payment_place.valid?
+      @payment_place.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment_place).permit(:post_code, :prefecture_id, :city, 
+                                          :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/payment_place.rb
+++ app/models/payment_place.rb
@@ -1,19 +1,19 @@
 class PaymentPlace
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :block, :building, :phone_number
+  attr_accessor :post_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :post_dode, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを含む半角数字7桁で入力してください" }
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "はハイフンを含む半角数字7桁で入力してください" }
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角で記入してください" }
     validates :block, format: { with: /\A[ぁ-んァ-ン一-龥々a-zA-Z0-9-ー]/, message: "は全角日本語および半角英数で記入してください" }
-    validates :phone_number, format: { with: /\A[0-9]{10, 11}\z/, message: "は半角数字（ハイフンなし）で記入してください" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は半角数字（ハイフンなし）で記入してください" }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
   validates :building, format: { with: /\A[ぁ-んァ-ン一-龥々a-zA-Z0-9-ー]/, message: "は全角日本語および半角英数で記入してください" }
 
   def save
-   
-    payment = Payment.create(item_id: @item.id, user_id: current_user.id)
-    Place.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, payment_id: payment.id)
+    payment = Payment.create(item_id: item_id, user_id: user_id)
+    Place.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block,
+                 building: building, phone_number: phone_number, payment_id: payment.id)
   end
 end
