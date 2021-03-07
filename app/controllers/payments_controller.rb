df1afc46f89699665cac1ba0e2f6618412a40754
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-03-07 14:13:19 +0900

    購入を、ログイン状態の出品者でないユーザーに限定

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,11 +1,13 @@
 class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :if_current_user
+
   def new
-    @item = Item.find(params[:item_id])
     @payment_place = PaymentPlace.new
   end
 
-  def create
-    @item = Item.find(params[:item_id])
+  def create  
     @payment_place = PaymentPlace.new(payment_params)
     if @payment_place.valid?
       @payment_place.save
@@ -21,4 +23,15 @@ class PaymentsController < ApplicationController
     params.require(:payment_place).permit(:post_code, :prefecture_id, :city, 
                                           :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def if_current_user
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
 end
