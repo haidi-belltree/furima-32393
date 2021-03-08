efe52cb0d96d99d2611f8649bed53c6b971dbe1b
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-03-08 15:56:26 +0900

    PAY.JPを使うための準備

---
+++ app/javascript/card.js
@@ -0,0 +1,9 @@
+const pay = () => {
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+  });
+};
+
+window.addEventListener("load", pay)
