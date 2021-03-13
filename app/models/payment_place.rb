34cafe2803094a17b9f302782494617f648d004e
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-03-13 18:18:44 +0900

    購入についてのバリデーションの記述をまとめる

--- app/models/payment_place.rb
+++ app/models/payment_place.rb
@@ -7,9 +7,11 @@ class PaymentPlace
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角で記入してください' }
     validates :block, format: { with: /\A[ぁ-んァ-ン一-龥々0-9-ー]/, message: 'は全角日本語および半角数字で記入してください' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は半角数字10〜11桁（ハイフンなし）で記入してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+    validates :token, :user_id, :item_id, :item_price, presence: true
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
-  validates :token, :user_id, :item_id, :item_price, presence: true
+#  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+#  validates :token, :user_id, :item_id, :item_price, presence: true
 
   def save
     payment = Payment.create(item_id: item_id, user_id: user_id)
