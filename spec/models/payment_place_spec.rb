53c95e47a1c5ac07b4258ea6de567b0f40e910a9
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-04-04 14:29:53 +0900

    テストコード修正

--- spec/models/payment_place_spec.rb
+++ spec/models/payment_place_spec.rb
@@ -24,88 +24,87 @@ RSpec.describe PaymentPlace, type: :model do
     it '郵便番号が空では保存できない' do
       @payment_place.post_code = ''
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("Post code can't be blank", 'Post code はハイフンを含む半角数字7桁で入力してください')
+      expect(@payment_place.errors.full_messages).to include('郵便番号を入力してください')
     end
     it '都道府県が選択されていなければ保存できない' do
       @payment_place.prefecture_id = 1
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('Prefecture を選択してください')
+      expect(@payment_place.errors.full_messages).to include('都道府県を選択してください')
     end
     it '市町村が空では保存できない' do
       @payment_place.city = ''
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("City can't be blank", 'City は全角で記入してください')
+      expect(@payment_place.errors.full_messages).to include('市区町村を入力してください')
     end
     it '番地が空では保存できない' do
       @payment_place.block = ''
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("Block can't be blank", 'Block は全角日本語および半角数字で記入してください')
+      expect(@payment_place.errors.full_messages).to include('番地を入力してください')
     end
     it '電話番号が空では保存できない' do
       @payment_place.phone_number = ''
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("Phone number can't be blank",
-                                                             'Phone number は半角数字10〜11桁（ハイフンなし）で記入してください')
+      expect(@payment_place.errors.full_messages).to include('電話番号を入力してください')
     end
     it 'user_idがなければ保存できない' do
       @payment_place.user_id = ''
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("User can't be blank")
+      expect(@payment_place.errors.full_messages).to include('Userを入力してください')
     end
     it 'item_idがなければ保存できない' do
       @payment_place.item_id = ''
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("Item can't be blank")
+      expect(@payment_place.errors.full_messages).to include('Itemを入力してください')
     end
     it 'item_priceがなければ保存できない' do
       @payment_place.item_price = ''
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("Item price can't be blank")
+      expect(@payment_place.errors.full_messages).to include('Item priceを入力してください')
     end
     it '郵便番号は半角数字でなければ保存できない' do
       @payment_place.post_code = '１２３-４５６７'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('Post code はハイフンを含む半角数字7桁で入力してください')
+      expect(@payment_place.errors.full_messages).to include('郵便番号はハイフンを含む半角数字7桁で入力してください')
     end
     it '郵便番号はハイフンを含まなければ保存できない' do
       @payment_place.post_code = '1234567'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('Post code はハイフンを含む半角数字7桁で入力してください')
+      expect(@payment_place.errors.full_messages).to include('郵便番号はハイフンを含む半角数字7桁で入力してください')
     end
     it '郵便番号は3桁＋4桁でなければ保存できない' do
       @payment_place.post_code = '1234-567'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('Post code はハイフンを含む半角数字7桁で入力してください')
+      expect(@payment_place.errors.full_messages).to include('郵便番号はハイフンを含む半角数字7桁で入力してください')
     end
     it '市町村は全角でなければ保存できない' do
       @payment_place.city = 'ﾖｺﾊﾏｼﾐﾄﾞﾘｸ'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('City は全角で記入してください')
+      expect(@payment_place.errors.full_messages).to include('市区町村は全角日本語で記入してください')
     end
     it '市町村は全角でも、日本語でなければ保存できない' do
       @payment_place.city = 'Ｍｉｄｏｒｉ－Ｗａｒｄ　Ｙｏｋｏｈａｍａ－Ｃｉｔｙ'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('City は全角で記入してください')
+      expect(@payment_place.errors.full_messages).to include('市区町村は全角日本語で記入してください')
     end
     it '番地は全角日本語および半角数字以外では保存できない' do
       @payment_place.block = 'ΘψΔη１ー１ー１'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('Block は全角日本語および半角数字で記入してください')
+      expect(@payment_place.errors.full_messages).to include('番地は全角日本語および半角数字で記入してください')
     end
     it '電話番号は10桁または11桁でなければ保存できない' do
       @payment_place.phone_number = '090112233445566778899'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('Phone number は半角数字10〜11桁（ハイフンなし）で記入してください')
+      expect(@payment_place.errors.full_messages).to include('電話番号は半角数字10〜11桁（ハイフンなし）で記入してください')
     end
     it '電話番号は10桁または11桁でも半角数字でなければ保存できない' do
       @payment_place.phone_number = '０９０１２３４５６７８'
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include('Phone number は半角数字10〜11桁（ハイフンなし）で記入してください')
+      expect(@payment_place.errors.full_messages).to include('電話番号は半角数字10〜11桁（ハイフンなし）で記入してください')
     end
     it 'tokenが空では保存できない' do
       @payment_place.token = nil
       @payment_place.valid?
-      expect(@payment_place.errors.full_messages).to include("Token can't be blank")
+      expect(@payment_place.errors.full_messages).to include('クレジットカード情報を入力してください')
     end
   end
 end
