af734c42bfc0b1f11818f8f61b071b47876830ab
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-04-04 14:28:43 +0900

    エラーメッセージを自然な日本語に調整

--- app/models/payment_place.rb
+++ app/models/payment_place.rb
@@ -4,7 +4,7 @@ class PaymentPlace
 
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフンを含む半角数字7桁で入力してください' }
-    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角で記入してください' }
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'は全角日本語で記入してください' }
     validates :block, format: { with: /\A[ぁ-んァ-ン一-龥々0-9-ー]/, message: 'は全角日本語および半角数字で記入してください' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は半角数字10〜11桁（ハイフンなし）で記入してください' }
     validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
