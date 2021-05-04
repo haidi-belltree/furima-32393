5bc49328224ba52531ea15675d22793dd7b0b583
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-05-04 17:12:55 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   devise_for :users
   root to: "items#index"
 
   resources :items do
     resources :payments, only: [:new, :create]
   end
+
+  resources :cards, only: [:new, :create]
 end
