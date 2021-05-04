f6616cadccaa2f98fd6e6e6371a8dbe69d88907f
haidi-belltree <3p1erfv.mbah8yj4higu@gmail.com>
2021-05-04 17:20:44 +0900

    cardsコントローラ記述

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,29 @@
+class CardsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    if @card.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def card_params
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    @customer = Payjp::Customer.create(
+      description: 'test',
+      card: params[:card_token]
+    )
+    params.permit(:card_token).merge(customer_token: @customer.id, user_id: current_user.id)
+  end
+
+end
